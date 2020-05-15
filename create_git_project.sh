69b1a017302479c6250e3d2fd45c07d5bd77489d
Marc Loftus <marc@webmarcit.com>
2020-05-15 19:17:04 +0100

    Create create_git_project.sh
    Create and initialise a github project using a raspberry pi drive

---
+++ create_git_project.sh
@@ -0,0 +1,10 @@
+if [ $# -lt 1 ] ; then
+        echo "Usage: ./create_project.sh <project_name>";
+else
+        PRJ_NAME="$1.git"
+        HOSTIP="$( hostname -I | cut -f1 -d' ')"
+        COMMAND="git remote add pi pi@$HOSTIP:$HOME/usbdrv/$PRJ_NAME"
+        mkdir /github/$PRJ_NAME || echo "Project Already Exists..."
+        cd /github/$PRJ_NAME
+        git init --bare && echo "Use this command to add origin $COMMAND"
+fi
